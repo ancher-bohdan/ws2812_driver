6743ec7c14304b3e0a9a8730164c375486b26df4
Bohdan Chupys <chupys.bohdan@gmail.com>
2020-01-12 02:46:36 +0200

    [TASK] Add base code for driver layer

---
+++ inc/driver/led_driver.h
@@ -0,0 +1,82 @@
+#ifndef __LED_DRIVER__
+#define __LED_DRIVER__
+
+#include <stdint.h>
+
+#include "ws_config.h"
+
+#define EOK             0
+#define ENOMEM          -1
+#define EINIT           -2
+#define EPARAM          -3
+
+#ifdef  USE_FULL_ASSERT
+  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+
+  void assert_failed(uint8_t* file, uint32_t line);
+#else
+  #define assert_param(expr) ((void)0U)
+#endif
+
+struct ws2812_driver_private;
+struct __dma_buffer;
+
+enum driver_state {
+    DR_STATE_IDLE = 0,
+    DR_STATE_SUSPEND,
+    DR_STATE_RUNNING
+};
+
+enum driver_buffer_state {
+    DRBUF_STATE_FREE = 0,
+    DRBUF_STATE_BUSY
+};
+
+struct color_representation {
+    uint16_t first;
+    uint8_t second;
+    uint8_t third;
+};
+
+struct driver_buffer_node {
+    struct __dma_buffer *buffer;
+    struct color_representation *color;
+    struct driver_buffer_node *next;
+    enum driver_buffer_state state;
+};
+
+struct ws2812_operation_fn_table {
+    void (*hw_start_dma) (void *ptr, uint16_t size);
+    void (*hw_stop_dma) ();
+    void (*hw_start_timer) ();
+    void (*hw_stop_timer) ();
+};
+
+struct ws2812_driver {
+    
+    struct driver_buffer_node *read;
+    struct driver_buffer_node *write;
+    struct driver_buffer_node *start;
+    uint16_t buffer_size;
+    uint8_t buffer_count;
+
+    enum driver_state state;
+
+    struct ws2812_driver_private *priv;
+    struct ws2812_operation_fn_table *fn_table;
+    
+    struct color_representation color[BUFFER_COUNT][BUFFER_SIZE];
+
+    void (*timer_interrupt)(struct ws2812_driver *driver );
+    void (*dma_interrupt)(struct ws2812_driver *driver );
+    
+    void (*driver_start)(struct ws2812_driver *driver);
+    void (*driver_stop)(struct ws2812_driver *driver);
+    void (*driver_suspend)(struct ws2812_driver *driver);
+    void (*driver_resume)(struct ws2812_driver *driver);
+
+};
+
+int ws2812_driver_init(struct ws2812_operation_fn_table *fn, struct ws2812_driver *driver);
+
+#endif //__LED_DRIVER__
---
+++ src/driver/ws2812_driver.c
@@ -0,0 +1,163 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "driver/led_driver.h"
+#include "driver/private.h"
+
+void dma_interrupt_routine(struct ws2812_driver *driver);
+void tim_interrupt_routine(struct ws2812_driver *driver);
+void ws2812_driver_suspend(struct ws2812_driver *driver);
+void ws2812_driver_resume(struct ws2812_driver *driver);
+void ws2812_driver_start(struct ws2812_driver *driver);
+void ws2812_driver_stop(struct ws2812_driver *driver);
+
+
+static bool check_all_buffer_node_state(struct ws2812_driver *driver, enum driver_buffer_state expected_state)
+{
+    struct driver_buffer_node *iterator = driver->start;
+    bool result = (iterator->state == expected_state);
+
+    while(result && (iterator->next != driver->start))
+    {
+        iterator = iterator->next;
+        result = (iterator->state == DRBUF_STATE_FREE);
+    }
+    return result;
+}
+
+static struct driver_buffer_node **__alloc_ring_buffer(struct ws2812_driver *driver, struct driver_buffer_node **prev)
+{
+    static int recursion_count = 0;
+
+    *prev = (struct driver_buffer_node *)malloc(sizeof(struct driver_buffer_node));
+
+    if(*prev == NULL)
+        return NULL;
+
+    (*prev)->buffer = driver->priv->dma_buffer[recursion_count];
+    (*prev)->color = driver->color[recursion_count];
+    (*prev)->state = DRBUF_STATE_FREE;
+
+    if(recursion_count != (BUFFER_COUNT - 1)) {
+        recursion_count++;
+        return __alloc_ring_buffer(driver, &((*prev)->next));
+    } else {
+        return prev;
+    }
+}
+
+int ws2812_driver_init(struct ws2812_operation_fn_table *fn, struct ws2812_driver *driver)
+{
+    struct driver_buffer_node **tmp = NULL;
+
+    if(fn == NULL)
+    {
+        return EPARAM;
+    }
+
+    driver = (struct ws2812_driver *) malloc (sizeof(struct ws2812_driver));
+    if(driver == NULL)
+    {
+        return ENOMEM;
+    }
+
+    driver->state = DR_STATE_IDLE;
+    driver->buffer_size = BUFFER_SIZE;
+    driver->buffer_count = BUFFER_COUNT;
+    driver->fn_table = fn;
+
+    driver->driver_start = ws2812_driver_start;
+    driver->driver_stop = ws2812_driver_stop;
+    driver->driver_resume = ws2812_driver_resume;
+    driver->driver_suspend = ws2812_driver_suspend;
+
+    driver->priv = (struct ws2812_driver_private *)malloc(sizeof(struct ws2812_driver_private));
+
+    if(driver->priv == NULL)
+    {
+        free(driver);
+        return ENOMEM;
+    }
+
+    tmp = __alloc_ring_buffer(driver, &(driver->start));
+
+    if(tmp == NULL)
+    {
+        //TODO: need to clean all driver_buffer_node, that have been created.
+        free(driver->priv);
+        free(driver);
+    }
+
+    (*tmp)->next = driver->start;
+    driver->write = driver->start;
+    driver->read = driver->start;
+
+    return EOK;
+}
+
+void dma_interrupt_routine(struct ws2812_driver *driver)
+{
+    assert_param(driver->read->state == DRBUF_STATE_BUSY);
+
+    driver->read->state = DRBUF_STATE_FREE;
+    driver->read = driver->read->next;
+    
+    if(driver->read->state != DRBUF_STATE_BUSY )
+    {
+        if(check_all_buffer_node_state(driver, DRBUF_STATE_FREE))
+        {
+            driver->driver_suspend(driver);
+        }
+        else
+        {
+            assert_param(0);
+        }
+        
+    }
+}
+
+void tim_interrupt_routine(struct ws2812_driver *driver)
+{
+    if(check_all_buffer_node_state(driver, DRBUF_STATE_BUSY))
+    {
+        driver->driver_resume(driver);
+    }
+}
+
+void ws2812_driver_suspend(struct ws2812_driver *driver)
+{
+    driver->fn_table->hw_stop_dma();
+
+    driver->state = DR_STATE_SUSPEND;
+
+    driver->fn_table->hw_start_timer();
+}
+
+void ws2812_driver_resume(struct ws2812_driver *driver)
+{
+    driver->fn_table->hw_stop_timer();
+
+    driver->state = DR_STATE_RUNNING;
+
+    driver->fn_table->hw_start_dma(driver->read->buffer, driver->buffer_count * driver->buffer_size * WORDS_PER_LED);
+}
+
+void ws2812_driver_start(struct ws2812_driver *driver)
+{
+    driver->state = DR_STATE_SUSPEND;
+    
+    driver->fn_table->hw_start_timer();
+}
+
+void ws2812_driver_stop(struct ws2812_driver *driver)
+{
+    if(driver->state == DR_STATE_RUNNING)
+    {
+        driver->fn_table->hw_stop_dma();
+    }
+    else if(driver->state == DR_STATE_SUSPEND)
+    {
+        driver->fn_table->hw_stop_timer();
+    }
+    driver->state = DR_STATE_IDLE;
+}
